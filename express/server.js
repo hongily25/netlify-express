22b7231ac76f7e4a8c13e00e3b83c66534de5c96
Emily Lam <elam@bomgar.com>
2019-06-19 21:13:12 -0700

    testing em

--- express/server.js
+++ express/server.js
@@ -4,17 +4,21 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
+const app = express.app();
+app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+app.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+app.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.get('/em', (req, res) => {
+  res.send('hello world');
+})
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', app);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
